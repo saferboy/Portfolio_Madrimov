ca94778794b8f0a73bdfd822c6d548383a6630a0
saferboy <muxtordev@gmail.com>
2023-04-10 10:31:39 +0500

    all fuctions of updated experience are over

--- src/api/experience.ts
+++ src/api/experience.ts
@@ -4,12 +4,13 @@ import { Router } from "express"
 
 import createExperience from "@controller/experience/create-experience"
 import findAllExperience from "@controller/experience/findAll-experience"
+import updateExperience from "@controller/experience/update-experience"
 
 
 const router = Router()
 
     .post('/', createExperience)
     .get('/', findAllExperience)
-
+    .patch('/:id', updateExperience)
 
 export default router
--- src/controller/experience/update-experience.ts
+++ src/controller/experience/update-experience.ts
@@ -1,1 +1,38 @@
 import { Request, Response, NextFunction } from "express";
+import { updateExperience, findOne } from "@service/experience.service";
+import { ExperinceDto } from "@model/experince-model";
+
+
+export default async (req: Request, res: Response, next: NextFunction) => {
+
+    try {
+
+        const id = +req.params.id
+
+        const data: ExperinceDto = req.body
+
+        const find = await findOne(id)
+
+        if (!find) {
+            return res.status(400).json({
+                message: `There is no information available for this ${id}`
+            })
+        }
+
+        const newBody = await updateExperience(id, data)
+
+        return res.status(200).json({
+            message: `The Experience has been updated for this ${id}`,
+            experience: {
+                id: newBody.id,
+                year: newBody.year,
+                description: newBody.description
+            }
+        })
+
+    } catch (error) {
+        next(error)
+    }
+
+
+}
